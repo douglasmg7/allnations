1365bc6b03fc7e7628a1ed6bbc126f843c1a330a
douglasmg7 <douglasmg7@gmail.com>
2020-08-12 09:54:31 -0300

    loggin in the same format that others zunka programs

--- src/logger.rs
+++ src/logger.rs
@@ -19,24 +19,44 @@ impl log::Log for Logger {
             let message: String;
             match record.level() {
                 log::Level::Error => {
-                    message = format!("[{}] [error] {}", record.target(), record.args());
+                    message = format!(
+                        "{} [{}] [error] {}",
+                        Local::now().format("%Y/%m/%d %H:%M:%S%.6f"),
+                        record.target(),
+                        record.args()
+                    );
                 }
                 log::Level::Warn => {
-                    message = format!("[{}] [warn] {}", record.target(), record.args());
+                    message = format!(
+                        "{} [{}] [warn] {}",
+                        Local::now().format("%Y/%m/%d %H:%M:%S%.6f"),
+                        record.target(),
+                        record.args()
+                    );
                 }
                 log::Level::Info => {
                     message = format!(
                         "{} [{}] [info] {}",
-                        Local::now().format("%Y-%m-%dT%H:%M:%S"),
+                        Local::now().format("%Y/%m/%d %H:%M:%S%.6f"),
                         record.target(),
                         record.args()
                     );
                 }
                 log::Level::Debug => {
-                    message = format!("[{}] [debug] {}", record.target(), record.args());
+                    message = format!(
+                        "{} [{}] [debug] {}",
+                        Local::now().format("%Y/%m/%d %H:%M:%S%.6f"),
+                        record.target(),
+                        record.args()
+                    );
                 }
                 log::Level::Trace => {
-                    message = format!("[{}] [trace] {}", record.target(), record.args());
+                    message = format!(
+                        "{} [{}] [trace] {}",
+                        Local::now().format("%Y/%m/%d %H:%M:%S%.6f"),
+                        record.target(),
+                        record.args()
+                    );
                 }
             }
             println!("{}", message);
