d053ab547f8d72188f15b46e4ecd348eed7fc77c
douglasmg7 <douglasmg7@gmail.com>
2020-08-20 23:40:10 -0300

    fetch-xml

--- bin/fetch-xml-products.sh
+++ bin/fetch-xml-products.sh
@@ -9,18 +9,44 @@
 XML_PATH=$ZUNKAPATH/xml/allnations
 mkdir -p $XML_PATH
 
+NOW=$(date +%Y-%m-%dT%H:%M:%S-03:00)
+
 # Last downloaded XML file.
-F_LAST=$XML_PATH/allnations-products.xml
+FILE_LAST=$XML_PATH/allnations_products.xml
+
+# Will not download products information if last file not processed yet.
+if [ -f $FILE_LAST ]; then
+    echo "Nothing to do, last file wasn't processed yet."
+    exit 0
+fi
+
+FILE_LAST_TIME_PRODUCT_WAS_DOWNLOADED=$XML_PATH/allnations_products_last_time_download.time
+if [[ -f $FILE_LAST_TIME_PRODUCT_WAS_DOWNLOADED ]]; then
+    LAST_TIME_PRODUCT_WAS_DOWNLOADED=$(cat $FILE_LAST_TIME_PRODUCT_WAS_DOWNLOADED)
+    echo Last time products was downloaded: $LAST_TIME_PRODUCT_WAS_DOWNLOADED
+else
+    LAST_TIME_PRODUCT_WAS_DOWNLOADED=2018-01-01T03:00:00-03:00
+    echo Products was never downloaded, using: $LAST_TIME_PRODUCT_WAS_DOWNLOADED
+fi
+
 # XML file backup.
-F_BACKUP=$XML_PATH/allnations-products-$(date +%Y-%h-%d-%H%M%S).xml
-# Last time XML file was downloaded.
-LAST_DATE=2020-07-18T09:00:00-03:00
+FILE_BACKUP=$XML_PATH/allnations_products_${NOW}.xml
 
 # Download xml file.
-# url = 'http://wspub.allnations.com.br/wsIntEstoqueClientes/ServicoReservasPedidosExt.asmx/RetornarListaProdutos?CodigoCliente=000&Senha=000&Data=2016-08-30T09:00:00-03:00';
-URL="http://wspub.allnations.com.br/wsIntEstoqueClientesV2/ServicoReservasPedidosExt.asmx/RetornarListaProdutosEstoque?CodigoCliente=${ALLNATIONS_USER}&Senha=${ALLNATIONS_PASS}&Data=${LAST_DATE}"
-# echo $URL
-curl -v $URL > $F_BACKUP
+# URL="http://wspub.allnations.com.br/wsIntEstoqueClientesV2/ServicoReservasPedidosExt.asmx/RetornarListaProdutosEstoque?CodigoCliente=${ALLNATIONS_USER}&Senha=${ALLNATIONS_PASS}&Data=${LAST_TIME_PRODUCT_WAS_DOWNLOADED}"
+# curl -v $URL > $FILE_BACKUP
+echo asdfadsfasdfasdfasdfasdfasdçflkajsdçflajsçdlfkjasdlkfjaçsdlkfjaçlsdkfjçalskdjfçalksdjçflaksjdçflkajsçdlfkajçsdlfkjaçsldkfjçalsdkfjçalskdjfasldfkj > $FILE_BACKUP
 
-# Copy as last xml file.
-cp $F_BACKUP $F_LAST
+# Get created file size.
+if [[ -f $FILE_BACKUP ]]; then
+    FILE_SIZE=$(stat -c%s $FILE_BACKUP)
+else
+    FILE_SIZE=0
+fi
+
+# If a valid size, copy as last xml file to process.
+echo size: $FILE_SIZE
+if [[ "$FILE_SIZE" -gt "100" ]]; then
+    cp $FILE_BACKUP $FILE_LAST
+    echo $NOW > $FILE_LAST_TIME_PRODUCT_WAS_DOWNLOADED
+fi
