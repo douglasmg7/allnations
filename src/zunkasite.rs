1b5bc7b0415dcba72e09be20031122593f6633c3
douglasmg7 <douglasmg7@gmail.com>
2020-09-04 07:25:02 -0300

    get allnations zunka site products test working

--- src/zunkasite.rs
+++ src/zunkasite.rs
@@ -10,26 +10,21 @@ fn update_zunkasite_product(product: &Product, config: &Config) {
 }
 
 #[allow(dead_code)]
-#[tokio::main]
+// #[tokio::main]
 async fn get_all_allnations_products(config: &Config) -> Result<(), Box<dyn std::error::Error>> {
     let response = reqwest::Client::new()
-        .get("https://httpbin.org/basic-auth/user/passwd")
+        .get(&format!(
+            "{}/setup/products/allnations",
+            &config.zunkasite_host
+        ))
         .basic_auth(&config.zunkasite_user, Some(&config.zunkasite_pass))
         .send()
         .await
         .unwrap();
     println!("{}", response.status());
+    println!("Status: {}", response.status());
 
-    let res = reqwest::get(&format!(
-        "{}/setup/products/allnations",
-        &config.zunkasite_host
-    ))
-    .await
-    .unwrap();
-
-    println!("Status: {}", res.status());
-
-    let body = res.text().await.unwrap();
+    let body = response.text().await.unwrap();
     println!("Body:\n\n{}", body);
 
     Ok(())
@@ -40,42 +35,8 @@ mod test {
     #[tokio::test]
     async fn test_get_all_allnations_products() {
         let config = super::super::config::Config::new();
-        super::get_all_allnations_products(&config).await;
+        // tokio::run(super::get_all_allnations_products(&config).await);
+        super::get_all_allnations_products(&config).await.unwrap();
         println!("After function");
     }
 }
-
-// #[tokio::main]
-// async fn get_all_allnations_products(config: &Config) -> Result<(), Box<dyn std::error::Error>> {
-// // let resp = reqwest::get(&format!(
-// // "{}/setup/products/allnations",
-// // &config.zunkasite_host
-// // ))
-// // .await?
-// // .json::<HashMap<String, String>>()
-// // .await?;
-// // println!("{:#?}", resp);
-// // Ok(())
-
-// let res = reqwest::get(&format!(
-// "{}/setup/products/allnations",
-// &config.zunkasite_host
-// ))
-// .await?;
-// println!("Status: {}", res.status());
-
-// let body = res.text().await?;
-// println!("Body:\n\n{}", body);
-
-// Ok(())
-// }
-
-// #[tokio::main]
-// async fn main() -> Result<(), Box<dyn std::error::Error>> {
-// let resp = reqwest::get("https://httpbin.org/ip")
-// .await?
-// .json::<HashMap<String, String>>()
-// .await?;
-// println!("{:#?}", resp);
-// Ok(())
-// }
